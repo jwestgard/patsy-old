da8dd73339ce2d7e7372b007f050bed33ecfab7f
jwestgard <jwestgard@mac.com>
2018-06-27 01:31:30 -0400

    initial project setup

---
+++ db/populate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import csv
+import sqlite3
+import sys
+
+INSERT_CMD = '''INSERT INTO assets (dir, name, ext, bytes, mtime, moddate, md5)
+                VALUES ("{asset.dir}", "{asset.name}", "{asset.ext}",
+                         {asset.bytes}, {asset.mtime}, "{asset.moddate}",
+                         "{asset.md5}")'''
+INFILE =  sys.argv[1]
+OUTFILE = sys.argv[2]
+
+class Asset:
+    def __init__(self, Directory, Filename, Extension, Bytes, MTime, 
+                 Moddate, MD5):
+        self.dir     = Directory
+        self.name    = Filename
+        self.ext     = Extension
+        self.bytes   = Bytes
+        self.mtime   = MTime
+        self.moddate = Moddate
+        self.md5     = MD5
+
+conn = sqlite3.connect(OUTFILE)
+cursor = conn.cursor()
+cursor.execute('''DROP TABLE IF EXISTS assets''')
+cursor.execute('''CREATE TABLE assets(
+                    id INTEGER PRIMARY KEY,
+                    dir TEXT,
+                    name TEXT,
+                    ext TEXT,
+                    bytes INTEGER,
+                    mtime INTEGER,
+                    moddate TEXT,
+                    md5 TEXT)'''
+                    )
+
+with open(INFILE) as inputfile:
+    reader = csv.DictReader(inputfile, delimiter=',')
+    for d in reader:
+        a = Asset(**d)
+        cursor.execute(INSERT_CMD.format(asset=a))
+
+cursor.execute('SELECT * FROM assets')
+print(
+    "Loaded {} rows into assets table".format(len(cursor.fetchall()))
+    )
+
+conn.commit()
+conn.close()
---
+++ run.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+import sqlite3
+from flask import g
+
+DATABASE = 'db/patsy.sqlite'
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    db.row_factory = sqlite3.Row
+    return db
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return render_template('index.html')
+
+@app.route('/assets')
+def assets():
+    query = 'SELECT * FROM assets'
+    rows = query_db(query)
+    return render_template('assets.html', assets=rows)
+
+if __name__ == '__main__':
+    manager.run()
