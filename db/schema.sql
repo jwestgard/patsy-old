7f625dcdb390c412dccf404426a3cc5a3d4fc914
Joshua Westgard <jwestgard@mac.com>
2018-10-19 17:21:19 -0400

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,28 @@
 DROP TABLE IF EXISTS assets;
 DROP TABLE IF EXISTS batches;
 DROP TABLE IF EXISTS instances;
-CREATE TABLE assets(    id       INTEGER PRIMARY KEY,
-                        filename TEXT,
-                        ext      TEXT,
-                        bytes    INTEGER,
-                        mtime    INTEGER,
-                        date     TEXT,
-                        md5      TEXT,
-                        sha1     TEXT,
-                        sha256   TEXT
-                        );
-CREATE TABLE batches(   id       INTEGER PRIMARY KEY,
-                        filename TEXT,
-                        number   INTEGER,
-                        date     TEXT
-                        );
-CREATE TABLE instances( id       INTEGER PRIMARY KEY,
-                        filename TEXT,
-                        path     TEXT,
-                        location TEXT,
-                        created  TEXT,
-                        batch_id INTEGER,
-                        asset_id INTEGER
-                        );
+
+CREATE TABLE assets(    
+    id       INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    bytes    INTEGER,
+    md5      TEXT,
+    sha1     TEXT,
+    sha256   TEXT
+    );
+
+CREATE TABLE batches(   
+    id       INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    filename TEXT,
+    number   INTEGER,
+    date     TEXT
+    );
+
+CREATE TABLE instances( 
+    id       INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+    filename TEXT,
+    path     TEXT,
+    location TEXT,
+    created  TEXT,
+    batch_id INTEGER,
+    asset_id INTEGER
+    );
